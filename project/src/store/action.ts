88410c5ebd6c0dc809a7ef69b409aa7fc0c46076
B3rzxd3v <d3vilc0re@gmail.com>
2022-08-03 02:18:17 +0700

    удалил лишние action

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -1,7 +1,3 @@
 import { createAction } from '@reduxjs/toolkit';
 
-export const changeCity = createAction<{city: string}>('changeCity');
-
-export const changeSortBy = createAction<{sortBy: string}>('changeSortBy');
-
 export const setErrorMessage = createAction<string | null>('setErrorMessage');
