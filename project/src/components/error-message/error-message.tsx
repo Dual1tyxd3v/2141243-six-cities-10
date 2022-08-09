18f87e14763f3e2ca7b5e7736c5103ae2423e742
B3rzxd3v <d3vilc0re@gmail.com>
2022-08-10 01:55:20 +0700

    Добавил тест для ErrorMessage

--- project/src/components/error-message/error-message.tsx
+++ project/src/components/error-message/error-message.tsx
@@ -5,7 +5,7 @@ import './error-message.css';
 function ErrorMessage(): JSX.Element | null {
   const error = useAppSelector(getErrorMessage);
   return error
-    ? <div className="error-message">{error}</div>
+    ? <div className="error-message" data-testid="errorBlock">{error}</div>
     : null;
 }
 
