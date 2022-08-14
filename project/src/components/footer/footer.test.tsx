4afa206e72566b17df7c60b66960595031726752
B3rzxd3v <d3vilc0re@gmail.com>
2022-08-14 13:14:03 +0700

    Добавил тест для Footer

--- project/src/components/footer/footer.test.tsx
+++ project/src/components/footer/footer.test.tsx
@@ -1,7 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
+import { Route, Routes } from 'react-router-dom';
+import { AppRoute } from '../../const';
 import HistoryRouter from '../history-route/history-route';
 import Footer from './footer';
+import userEvent from '@testing-library/user-event';
 
 const history = createMemoryHistory();
 
@@ -15,5 +18,25 @@ describe('Component: Footer', () => {
 
     expect(screen.getByRole('link')).toBeInTheDocument();
     expect(screen.getByAltText('6 cities logo')).toBeInTheDocument();
+  });
+
+  it('shoud redirect to "MainScreen" when user click link', async () => {
+    history.push(AppRoute.Login);
+
+    render(
+      <HistoryRouter history={history}>
+        <Routes>
+          <Route path={AppRoute.Login} element={<Footer />} />
+          <Route path={AppRoute.Main} element={<h1>main screen</h1>} />
+        </Routes>
+      </HistoryRouter>
+    );
+
+    expect(screen.queryByText(/main screen/i)).not.toBeInTheDocument();
+
+    await userEvent.click(screen.getByRole('link'));
+
+    expect(history.location.pathname).toBe(AppRoute.Main);
+    expect(screen.getByText(/main screen/i)).toBeInTheDocument();
   });
 });
