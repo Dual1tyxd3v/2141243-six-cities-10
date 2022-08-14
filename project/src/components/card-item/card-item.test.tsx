def288f9c942400d8294a056a49d384d452575b2
B3rzxd3v <d3vilc0re@gmail.com>
2022-08-14 13:07:48 +0700

    Добавил часть тестов для cardItem

--- project/src/components/card-item/card-item.test.tsx
+++ project/src/components/card-item/card-item.test.tsx
@@ -2,21 +2,25 @@ import { configureMockStore } from '@jedmao/redux-mock-store';
 import { render, screen } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
 import { Provider } from 'react-redux';
-import { AuthorizationStatus } from '../../const';
-import { makeFakeOffer } from '../../utils/mocks';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import { makeFakeOffer, makeFakeOffers } from '../../utils/mocks';
 import HistoryRouter from '../history-route/history-route';
 import CardItem from './card-item';
+import userEvent from '@testing-library/user-event';
+import { Route, Routes } from 'react-router-dom';
 
 const mockStore = configureMockStore();
 const history = createMemoryHistory();
+const mockOffer = makeFakeOffer();
+const mockOffers = makeFakeOffers().concat(mockOffer);
+mockOffer.isFavorite = true;
+const store = mockStore({
+  USER: {authorizationStatus: AuthorizationStatus.Auth},
+  DATA: {offers: mockOffers}
+});
 
 describe('Component: CardItem', () => {
   it('should render correctly', () => {
-    const mockOffer = makeFakeOffer();
-    mockOffer.isFavorite = true;
-    const store = mockStore({
-      USER: {authorizationStatus: AuthorizationStatus.Auth},
-    });
 
     render(
       <Provider store={store}>
@@ -35,4 +39,85 @@ describe('Component: CardItem', () => {
     expect(screen.getByRole('button')).toBeInTheDocument();
     expect(screen.getByRole('button').classList.contains('place-card__bookmark-button--active')).toBe(true);
   });
+
+  it('onMouseOverHandler should called when user "mouseEnter" on article', async () => {
+    const onMouseOverHandler = jest.fn();
+
+    render(
+      <Provider store={store}>
+        <HistoryRouter history={history}>
+          <CardItem
+            offer={mockOffer}
+            classPrefix='mockClass'
+            onActiveCard={onMouseOverHandler}
+          />
+        </HistoryRouter>
+      </Provider>
+    );
+
+    const article = screen.getByTestId('cardContainer');
+
+    await userEvent.hover(article);
+
+    expect(onMouseOverHandler).toBeCalledTimes(1);
+    expect(onMouseOverHandler).nthCalledWith(1, mockOffer, 'mouseenter');
+  });
+
+  it('onMouseOverHandler should called when user "mouseLeave" from article', async () => {
+    const onMouseOverHandler = jest.fn();
+
+    render(
+      <Provider store={store}>
+        <HistoryRouter history={history}>
+          <CardItem
+            offer={mockOffer}
+            classPrefix='mockClass'
+            onActiveCard={onMouseOverHandler}
+          />
+        </HistoryRouter>
+      </Provider>
+    );
+
+    const article = screen.getByTestId('cardContainer');
+
+    await userEvent.unhover(article);
+
+    expect(onMouseOverHandler).toBeCalledTimes(1);
+    expect(onMouseOverHandler).nthCalledWith(1, mockOffer, 'mouseleave');
+  });
+
+  it('should redirect to /offer/id when user click Link', async () => {
+    history.push(AppRoute.Main);
+
+    render(
+      <Provider store={store}>
+        <HistoryRouter history={history}>
+          <Routes>
+            <Route path={AppRoute.Room} element={<h1>offer screen</h1>} />
+            <Route path={AppRoute.Main} element={
+              <CardItem
+                offer={mockOffer}
+                classPrefix='mockClass'
+                onActiveCard={jest.fn()}
+              />
+            }
+            />
+          </Routes>
+        </HistoryRouter>
+      </Provider>
+    );
+
+    expect(screen.queryByText(/offer screen/i)).not.toBeInTheDocument();
+
+    const link = screen.getAllByRole('link')[0];
+
+    await userEvent.click(link);
+
+    expect(screen.getByText(/offer screen/i)).toBeInTheDocument();
+    expect(history.location.pathname).toBe(`/offer/${mockOffer.id}`);
+  });
+
+  /* it('handleClick should redirect to /login when user click button with AuthStatus = "NO_AUTH"', async () => {
+
+  }); */
 });
