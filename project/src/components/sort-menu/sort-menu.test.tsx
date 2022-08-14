4db85c66f3396002763256477f5a626cf131217c
B3rzxd3v <d3vilc0re@gmail.com>
2022-08-14 23:25:00 +0700

    Добавил тест для SortMenu

--- project/src/components/sort-menu/sort-menu.test.tsx
+++ project/src/components/sort-menu/sort-menu.test.tsx
@@ -3,8 +3,11 @@ import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { sortMenuTabs } from '../../const';
 import SortMenu from './sort-menu';
+import userEvent from '@testing-library/user-event';
+import thunk from 'redux-thunk';
 
-const mockStore = configureMockStore();
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 describe('Component: SortMenu', () => {
   it('should render correctly', () => {
@@ -19,5 +22,27 @@ describe('Component: SortMenu', () => {
     );
 
     expect(screen.getByText(sortMenuTabs.Popular)).toBeInTheDocument();
+  });
+
+  it('should change sort method to "rated" when user click tab', async () => {
+    const onCloseMenu = jest.fn();
+    const store = mockStore({
+      APP: {sortBy: sortMenuTabs.Popular}
+    });
+
+    render(
+      <Provider store={store}>
+        <SortMenu onCloseMenu={onCloseMenu}/>
+      </Provider>
+    );
+
+    expect(store.getActions().length).toBe(0);
+
+    await userEvent.click(screen.getByText(sortMenuTabs.Rated));
+
+    expect(store.getActions().length).toBe(1);
+    expect(store.getActions()[0].type).toBe('APP/changeSortBy');
+    expect(store.getActions()[0].payload).toBe(sortMenuTabs.Rated);
+    expect(onCloseMenu).toBeCalledTimes(1);
   });
 });
